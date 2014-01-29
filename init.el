d654c48096bb86784f2ef8336cdedb48c08a7089
Ram Iyer <ramky.iyer@yahoo.com>
2014-01-29 01:04:54 -0800

    adding symlinks within the repo for erlang to allow separate brew cellars

--- init.el
+++ init.el
@@ -77,9 +77,12 @@
 
 ;; erlang
 ;; Erlang mode (installed via Homebrew)
-(setq load-path (cons "/usr/local/Cellar/erlang/R16B03/lib/erlang/lib/tools-2.6.13/emacs" load-path))
-(setq erlang-root-dir "/usr/local/Cellar/erlang/R16B03/lib/erlang/lib")
-(setq exec-path (cons "/usr/local/Cellar/erlang/R16B03/lib/erlang/bin" exec-path))
+;;(setq load-path (cons "/usr/local/Cellar/erlang/R16B03/lib/erlang/lib/tools-2.6.13/emacs" load-path))
+;;(setq erlang-root-dir "/usr/local/Cellar/erlang/R16B03/lib/erlang/lib")
+;;(setq exec-path (cons "/usr/local/Cellar/erlang/R16B03/lib/erlang/bin" exec-path))
+(setq load-path (cons "~/.emacs.d/erlang/emacs" load-path))
+(setq erlang-root-dir "~/.emacs.d/erlang/lib")
+(setq exec-path (cons "~/.emacs.d/erlang/bin" exec-path))
 (require 'erlang-start)
 (add-to-list 'auto-mode-alist '("\\.erl?$" . erlang-mode))
 
