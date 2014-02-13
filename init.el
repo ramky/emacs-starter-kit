9fb5b4bf676601e29a0f7f739e57171a2e5326f2
Ram Iyer <ramky.iyer@yahoo.com>
2014-02-13 15:56:08 -0800

    correcting python-mode config

--- init.el
+++ init.el
@@ -90,7 +90,9 @@
 (add-to-list 'auto-mode-alist '("\\.erl?$" . erlang-mode))
 
 (require 'python-mode)
-(add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
 ;; color-themes
 (require 'color-theme)
